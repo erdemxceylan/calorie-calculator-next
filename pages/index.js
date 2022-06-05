934fb300f4bbaca1fda7688d1fe0ce6d2563e77e
Erdem Ceylan <erdemxceylan@gmail.com>
2022-06-05 09:10:54 +0300

    initial data fetching with getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,64 @@
 import axios from 'axios';
 import Home from '../components/home/Home';
+import { CONSTANTS } from './api/constants';
 
 export default function HomePage(props) {
   return <Home nutrients={props.nutrients} dailyTargetValues={props.dailyTargetValues} />;
 }
 
 export async function getStaticProps() {
-  const nutrientsResponse = await axios.get('http://localhost:8080/nutrients');
-  const settingsResponse = await axios.get('http://localhost:8080/settings');
-  return { props: { nutrients: nutrientsResponse.data, dailyTargetValues: settingsResponse.data } };
+  // Fetching nutrients
+  let nutrients = [];
+
+  const nutrientsResponse = await axios.get(`${CONSTANTS.BASE_URL}/${CONSTANTS.NUTRIENTS}.json`);
+
+  for (const key in nutrientsResponse.data) {
+    nutrients.push({
+      id: key,
+      name: nutrientsResponse.data[key].name,
+      unit: nutrientsResponse.data[key].unit,
+      calories: nutrientsResponse.data[key].calories,
+      proteins: nutrientsResponse.data[key].proteins
+    });
+  }
+
+  // Fetching daily target values
+  let dataSettings = {};
+  let dailyCalorieTargetLowerBound = 0;
+  let dailyCalorieTargetUpperBound = 0;
+  let dailyProteinNeed = 0;
+
+  const settingsResponse = await axios.get(`${CONSTANTS.BASE_URL}/${CONSTANTS.SETTINGS}.json`);
+
+  for (const key in settingsResponse.data) {
+    dataSettings = {
+      dailyCalorieNeed: settingsResponse.data[key].dailyCalorieNeed,
+      weight: settingsResponse.data[key].weight,
+      fatRatio: settingsResponse.data[key].fatRatio,
+      fitnessGoal: settingsResponse.data[key].fitnessGoal
+    };
+  }
+
+  const fatlessWeight = dataSettings.weight * (100 - dataSettings.fatRatio) / 100;
+
+  switch (dataSettings.fitnessGoal) {
+    case CONSTANTS.WEIGHT_GAIN:
+      dailyCalorieTargetLowerBound = dataSettings.dailyCalorieNeed;
+      dailyCalorieTargetUpperBound = dataSettings.dailyCalorieNeed + 300;
+      dailyProteinNeed = fatlessWeight * 2;
+      break;
+    case CONSTANTS.WEIGHT_LOSS:
+      dailyCalorieTargetLowerBound = dataSettings.dailyCalorieNeed - 500;
+      dailyCalorieTargetUpperBound = dataSettings.dailyCalorieNeed;
+      dailyProteinNeed = fatlessWeight * 1.5;
+      break;
+  }
+
+  const dailyTargetValues = {
+    dailyCalorieTargetLowerBound,
+    dailyCalorieTargetUpperBound,
+    dailyProteinNeed
+  };
+
+  return { props: { nutrients, dailyTargetValues } };
 }
--- pages/nutrients.js
+++ pages/nutrients.js
@@ -1,11 +1,43 @@
 import axios from 'axios';
+import { useState } from 'react';
 import NutrientList from '../components/nutrients/NutrientList';
+import { CONSTANTS } from '../pages/api/constants';
 
 export default function NutrientPage(props) {
    return <NutrientList nutrients={props.nutrients} />;
 }
 
 export async function getStaticProps() {
-   const response = await axios.get('http://localhost:8080/nutrients');
-   return { props: { nutrients: response.data } };
+   // Fetching nutrients
+   let nutrients = [];
+
+   const response = await axios.get(`${CONSTANTS.BASE_URL}/${CONSTANTS.NUTRIENTS}.json`);
+
+   for (const key in response.data) {
+      nutrients.push({
+         id: key,
+         name: response.data[key].name,
+         unit: response.data[key].unit,
+         calories: response.data[key].calories,
+         proteins: response.data[key].proteins
+      });
+   }
+
+   // const [nutrients, setNutrients] = useState([]);
+
+   // axios.get(`${CONSTANTS.BASE_URL}/${CONSTANTS.NUTRIENTS}.json`).then(response => {
+   //    let loadedNutrients = [];
+   //    for (const key in response.data) {
+   //       loadedNutrients.push({
+   //          id: key,
+   //          name: response.data[key].name,
+   //          unit: response.data[key].unit,
+   //          calories: response.data[key].calories,
+   //          proteins: response.data[key].proteins
+   //       });
+   //    }
+   //    setNutrients(loadedNutrients);
+   // });
+
+   return { props: { nutrients } };
 }
