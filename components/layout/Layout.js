1279888d6ad0289f01b3a0baca9dfa486d8b3569
Erdem Ceylan <erdemxceylan@gmail.com>
2022-06-05 04:21:06 +0300

    In Layout, Fragment is used instead of div

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,11 +1,12 @@
+import { Fragment } from 'react';
 import MainNavigation from './MainNavigation';
 import styles from './Layout.module.css';
 
 export default function Layout(props) {
    return (
-      <div>
+      <Fragment>
          <MainNavigation />
          <main className={styles.container}>{props.children}</main>
-      </div>
+      </Fragment>
    );
 }
