0f350e1f5fd2e2f6cf7fed65330447b9425f912d
Erdem Ceylan <erdemxceylan@gmail.com>
2022-06-11 04:51:22 +0300

    radio buttons component is revised

--- form/formik/Input.js
+++ form/formik/Input.js
@@ -1,18 +1,16 @@
 import { Fragment } from 'react';
 import { Field, ErrorMessage } from 'formik';
 import styles from './Formik.module.css';
+import cn from 'classnames';
 
 export default function Input(props) {
   const { name, label, errors, touched, ...rest } = props;
-  const invalid = errors[name] && touched[name] ? 'invalid' : null;
+  const invalid = errors[name] && touched[name] ? styles.invalid : null;
 
   return (
     <Fragment>
-      {/* <label htmlFor={name}> {label}</label> */}
-      <div className={invalid}>
-        <Field className={styles.input} name={name} {...rest} />
-        <ErrorMessage component='div' name={name} />
-      </div>
+      <Field className={cn(styles.input, invalid)} name={name} {...rest} />
+      <ErrorMessage component='div' className={styles['error-message']} name={name} />
     </Fragment>
   );
 }
--- form/formik/MyRadioButton.js
+++ form/formik/MyRadioButton.js
@@ -3,42 +3,42 @@ import { Field, ErrorMessage } from 'formik';
 import { RadioButton } from 'primereact/radiobutton';
 import { CONSTANTS } from '../../global/constants';
 import styles from './Formik.module.css';
-import cn from 'classnames';
 
 export default function MyRadioButton(props) {
-   const { name, errors, touched } = props;
-   const invalid = errors[name] && touched[name] ? 'invalid' : null;
+   const { name } = props;
 
    return (
-      <div className={cn(styles.radio, invalid)}>
-         <Field name={name} >
-            {({ field }) => {
-               return (
-                  <Fragment>
-                     <h4>Fitness Goal</h4>
-                     <div className='p-field-radiobutton'>
-                        <RadioButton
-                           inputId={CONSTANTS.WEIGHT_GAIN}
-                           {...field}
-                           value={CONSTANTS.WEIGHT_GAIN}
-                           checked={field.value === CONSTANTS.WEIGHT_GAIN}
-                        />
-                        <label htmlFor={CONSTANTS.WEIGHT_GAIN}>{CONSTANTS.WEIGHT_GAIN}</label>
-                     </div>
-                     <div className='p-field-radiobutton'>
-                        <RadioButton
-                           inputId={CONSTANTS.WEIGHT_LOSS}
-                           {...field}
-                           value={CONSTANTS.WEIGHT_LOSS}
-                           checked={field.value === CONSTANTS.WEIGHT_LOSS}
-                        />
-                        <label htmlFor={CONSTANTS.WEIGHT_LOSS}>{CONSTANTS.WEIGHT_LOSS}</label>
-                     </div>
-                  </Fragment>
-               );
-            }}
-         </Field>
-         <ErrorMessage component='div' name={name} />
-      </div>
+      <Fragment>
+         <div className={styles.radio}>
+            <Field name={name} >
+               {({ field }) => {
+                  return (
+                     <Fragment>
+                        <h4>Fitness Goal</h4>
+                        <div className='p-field-radiobutton'>
+                           <RadioButton
+                              inputId={CONSTANTS.WEIGHT_GAIN}
+                              {...field}
+                              value={CONSTANTS.WEIGHT_GAIN}
+                              checked={field.value === CONSTANTS.WEIGHT_GAIN}
+                           />
+                           <label htmlFor={CONSTANTS.WEIGHT_GAIN}>{CONSTANTS.WEIGHT_GAIN}</label>
+                        </div>
+                        <div className='p-field-radiobutton'>
+                           <RadioButton
+                              inputId={CONSTANTS.WEIGHT_LOSS}
+                              {...field}
+                              value={CONSTANTS.WEIGHT_LOSS}
+                              checked={field.value === CONSTANTS.WEIGHT_LOSS}
+                           />
+                           <label htmlFor={CONSTANTS.WEIGHT_LOSS}>{CONSTANTS.WEIGHT_LOSS}</label>
+                        </div>
+                     </Fragment>
+                  );
+               }}
+            </Field>
+         </div>
+         <ErrorMessage component='div' className={styles['error-message']} name={name} />
+      </Fragment>
    );
 }
