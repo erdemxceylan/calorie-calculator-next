af8baad1f0bf37bd8f0a2b789a14a7a582e17696
Erdem Ceylan <erdemxceylan@gmail.com>
2022-07-04 17:02:04 +0300

    yup & formik implementation for input

--- form/formik/Input.js
+++ form/formik/Input.js
@@ -1,9 +1,7 @@
 import { Fragment } from 'react';
 import { Field, ErrorMessage } from 'formik';
-import InputText from 'primereact/inputtext';
-import MyInput from './MyInput';
+import { InputText } from 'primereact/inputtext';
 import styles from './Formik.module.css';
-import cn from 'classnames';
 
 export default function Input(props) {
   const { name, errors, touched, ...rest } = props;
@@ -12,67 +10,9 @@ export default function Input(props) {
   return (
     <Fragment>
       <Field name={name} >
-        {({ field }) => {
-          return (
-            <MyInput
-              className={invalid}
-              {...field}
-              {...rest}
-            />
-          );
-        }}
+        {({ field }) => <InputText className={invalid} {...field} {...rest} />}
       </Field>
       <ErrorMessage className={styles['error-message']} name={name} component='div' />
     </Fragment>
   );
 }
-
-// Previous Input
-
-// import { Fragment } from 'react';
-// import { Field, ErrorMessage } from 'formik';
-// import InputText from 'primereact/inputtext';
-// import MyInput from './MyInput';
-// import styles from './Formik.module.css';
-// import cn from 'classnames';
-
-// export default function Input(props) {
-//   const { name, errors, touched, ...rest } = props;
-//   const invalid = errors[name] && touched[name] ? styles.invalid : null;
-
-//   return (
-//     <Fragment>
-//       <Field name={name} >
-//         {({ field }) => {
-//           return (
-//             <MyInput
-//               className={invalid}
-//               {...field}
-//               {...rest}
-//             />
-//           );
-//         }}
-//       </Field>
-//       <ErrorMessage className={styles['error-message']} name={name} component='div' />
-//     </Fragment>
-//   );
-// }
-
-// Previous Input with copied input styles
-
-// import { Fragment } from 'react';
-// import { Field, ErrorMessage } from 'formik';
-// import styles from './Formik.module.css';
-// import cn from 'classnames';
-
-// export default function Input(props) {
-//   const { name, label, errors, touched, ...rest } = props;
-//   const invalid = errors[name] && touched[name] ? styles.invalid : null;
-
-//   return (
-//     <Fragment>
-//       <Field className={cn(styles.input, invalid)} name={name} {...rest} />
-//       <ErrorMessage component='div' className={styles['error-message']} name={name} />
-//     </Fragment>
-//   );
-// }
--- form/formik/MyInput.js
+++
@@ -1,9 +0,0 @@
-import { InputText } from 'primereact/inputtext';
-
-export default function MyInput(props) {
-   // console.log(props);
-   // const { name, onChange, ...rest } = props;
-   return (
-      <InputText {...props} />
-   );
-}
