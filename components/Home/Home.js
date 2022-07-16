6df5c510bc5274aebc21109eb78a03a5138d86ea
Erdem Ceylan <erdemxceylan@gmail.com>
2022-07-16 15:28:50 +0300

    data menu toggle logic

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import AddNutrientMenu from './components/AddNutrientMenu';
 import ConsumedNutrientsTable from './components/ConsumedNutrientsTable';
 import DataMenu from './components/DataMenu';
-import styles from './Home.module.css';
 
 export default function Home(props) {
    const isEmpty = useSelector(state => state.consumedNutrients.isEmpty);
@@ -11,7 +10,6 @@ export default function Home(props) {
    return (
       <Fragment>
          <DataMenu dataSettings={props.dataSettings} />
-         <div className={styles.divider}></div>
          <AddNutrientMenu nutrients={props.nutrients} />
          {!isEmpty && <ConsumedNutrientsTable dailyTargetValues={props.dailyTargetValues} />}
       </Fragment>
--- components/Home/components/DataMenu.js
+++ components/Home/components/DataMenu.js
@@ -3,9 +3,11 @@ import { Button } from 'primereact/button';
 import { CONSTANTS } from '../../../global/constants';
 import DataSettings from '../../modals/DataSettings';
 import styles from './DataMenu.module.css';
+import cn from 'classnames';
 
 export default function DataMenu({ dataSettings }) {
    const [displayDataSettings, setDisplayDataSettings] = useState(false);
+   const [toggleMenu, setToggleMenu] = useState(false);
 
    const data = [
       { label: CONSTANTS.DCN, value: dataSettings.dailyCalorieNeed, unit: 'kcal' },
@@ -14,9 +16,12 @@ export default function DataMenu({ dataSettings }) {
       { label: CONSTANTS.FITNESS_GOAL, value: dataSettings.fitnessGoal }
    ];
 
+   const visible = toggleMenu ? styles.visible : null;
+   const border = toggleMenu ? styles['border-bottom'] : styles['border-top'];
+
    return (
       <Fragment>
-         <div className={styles.container}>
+         <div className={cn(styles.container, visible)}>
             {data.map(group => (
                <div className={styles.group} key={group.label}>
                   <label>{group.label}:</label>
@@ -28,6 +33,13 @@ export default function DataMenu({ dataSettings }) {
                icon='pi pi-fw pi-cog'
                onClick={() => setDisplayDataSettings(true)}
             />
+         </div>
+         <div
+            className={cn(styles.toggle, border)}
+            onClick={() => setToggleMenu(state => !state)}
+            icon='pi pi-fw pi-angle-up'
+         >
+            {toggleMenu ? 'up' : 'down'}
          </div>
          <DataSettings
             visible={displayDataSettings}
