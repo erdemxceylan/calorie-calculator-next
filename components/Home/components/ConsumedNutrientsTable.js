fdaa3fe9fa5cc352feb59e261c44f87522015ce5
Erdem Ceylan <erdemxceylan@gmail.com>
2022-07-21 05:23:45 +0300

    numbers are added to consumed  nutrient list

--- components/Home/components/ConsumedNutrientsTable.js
+++ components/Home/components/ConsumedNutrientsTable.js
@@ -20,17 +20,63 @@ const CALORIES_TAKEN = 'caloriesTaken';
 const PROTEINS_TAKEN = 'proteinsTaken';
 
 export default function ConsumedNutrientsTable(props) {
-
    const consumedNutrients = useSelector(state => state.consumedNutrients.consumedNutrients);
+   const indexedConsumedNutrients = consumedNutrients.map((nutrient, index) => { return { index, ...nutrient }; });
    const auth = useContext(AuthContext);
    const dispatch = useDispatch();
 
-   const consumedNutrientsTableColumns = [
+   const consumedNutrientsColumnHeaders = [
       { field: NUTRIENT_NAME, header: NAME },
       { field: CONSUMED_QUANTITY, header: QUANTITY },
       { field: CALORIES_TAKEN, header: CALORIES },
       { field: PROTEINS_TAKEN, header: PROTEINS }
    ];
+
+   const consumedNutrientsColumns = consumedNutrientsColumnHeaders.map(({ field, header }) => {
+      if (field === CONSUMED_QUANTITY) {
+         return <Column
+            key={field}
+            field={field}
+            header={header}
+            body={rowData => labelField(rowData, field)}
+            editor={options => quantityEditor(options)}
+         />;
+      } else {
+         return <Column
+            key={field}
+            field={field}
+            header={header}
+            body={rowData => labelField(rowData, field)}
+         />;
+      }
+   });
+
+   const numberColumn = (
+      <Column
+         header='No'
+         headerStyle={{ width: '2rem', minWidth: '2rem' }}
+         bodyStyle={{ textAlign: 'center' }}
+         body={rowData => rowData.index + 1}
+      />
+   );
+
+   const consumedNutrientsTable = (
+      <DataTable
+         value={indexedConsumedNutrients}
+         editMode='row'
+         onRowEditComplete={rowEditCompletionHandler}
+         responsiveLayout='scroll'
+         footer={<Totals dailyTargetValues={props.dailyTargetValues} />}
+      >
+         {numberColumn}
+         {consumedNutrientsColumns}
+         <Column
+            header='Edit'
+            rowEditor headerStyle={{ width: '2rem', minWidth: '2rem' }}
+            bodyStyle={{ textAlign: 'center' }}
+         />
+      </DataTable>
+   );
 
    function labelField(rowData, field) {
       switch (field) {
@@ -84,40 +130,6 @@ export default function ConsumedNutrientsTable(props) {
          originalEvent.preventDefault();
       }
    }
-
-   const consumedNutrientsTable = (
-      <DataTable
-         value={consumedNutrients}
-         editMode='row'
-         onRowEditComplete={rowEditCompletionHandler}
-         responsiveLayout='scroll'
-         footer={<Totals dailyTargetValues={props.dailyTargetValues} />}
-      >
-         {consumedNutrientsTableColumns.map(({ field, header }) => {
-            if (field === CONSUMED_QUANTITY) {
-               return <Column
-                  key={field}
-                  field={field}
-                  header={header}
-                  body={rowData => labelField(rowData, field)}
-                  editor={options => quantityEditor(options)}
-               />;
-            } else {
-               return <Column
-                  key={field}
-                  field={field}
-                  header={header}
-                  body={rowData => labelField(rowData, field)}
-               />;
-            }
-         })}
-         <Column
-            header='Edit'
-            rowEditor headerStyle={{ width: '8rem', minWidth: '8rem' }}
-            bodyStyle={{ textAlign: 'center' }}
-         />
-      </DataTable>
-   );
 
    return (
       <Fragment>
