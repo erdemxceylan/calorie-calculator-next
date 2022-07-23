104ca27be27d7cc1a750f85af6fefdaf3fceba26
Erdem Ceylan <erdemxceylan@gmail.com>
2022-07-23 04:36:31 +0300

    ui/table components are implemented to nutrient list table

--- components/nutrients/NutrientList.js
+++ components/nutrients/NutrientList.js
@@ -1,13 +1,17 @@
 import { useRouter } from 'next/router';
 import { Fragment, useContext, useState } from 'react';
-import useHttpRequest from '../../hooks/use-http-request';
-import AddNewNutrient from '../modals/AddNewNutrient';
 import { Button } from 'primereact/button';
 import { DataTable } from 'primereact/datatable';
 import { InputText } from 'primereact/inputtext';
 import { Column } from 'primereact/column';
 import { CONSTANTS } from '../../global/constants';
 import AuthContext from '../../global/context/auth';
+import useHttpRequest from '../../hooks/use-http-request';
+import Numbers from '../../ui/table/columns/Numbers';
+import Edition from '../../ui/table/columns/Edition';
+import Deletion from '../../ui/table/columns/Deletion';
+import DeletionButton from '../../ui/table/button/Deletion';
+import AddNewNutrient from '../modals/AddNewNutrient';
 import styles from './NutrientList.module.css';
 import cn from 'classnames';
 
@@ -44,15 +48,6 @@ export default function NutrientList(props) {
       />
    );
 
-   const numberColumn = (
-      <Column
-         header='No'
-         headerStyle={{ width: '2rem', minWidth: '2rem' }}
-         bodyStyle={{ textAlign: 'center' }}
-         body={rowData => rowData.index + 1}
-      />
-   );
-
    const nutrientListColumns = nutrientListColumnHeaders.map(({ field, header }) => {
       return <Column
          key={field}
@@ -63,22 +58,14 @@ export default function NutrientList(props) {
       />;
    });
 
-   const editionColumn = (
-      <Column
-         header='Edit'
-         rowEditor headerStyle={{ width: '8rem', minWidth: '8rem' }}
-         bodyStyle={{ textAlign: 'center' }}
-      />
-   );
-
-   const deletionColumn = (
-      <Column
-         header='Delete'
-         headerStyle={{ width: '5rem', minWidth: '5rem' }}
-         bodyStyle={{ textAlign: 'center' }}
-         body={rowData => deletionButton(rowData)}
-      />
-   );
+   // const deletionColumn = (
+   //    <Column
+   //       header='Delete'
+   //       headerStyle={{ width: '5rem', minWidth: '5rem' }}
+   //       bodyStyle={{ textAlign: 'center' }}
+   //       body={rowData => deletionButton(rowData)}
+   //    />
+   // );
 
    function editor(options, field) {
       if ([CALORIES_FIELD, PROTEINS_FIELD].includes(field)) {
@@ -130,14 +117,8 @@ export default function NutrientList(props) {
       router.push(CONSTANTS.NUTRIENTS_PAGE);
    }
 
-   function deletionButton(rowData) {
-      return (
-         <Button
-            icon='pi pi-trash'
-            className={'deletion-button'}
-            onClick={deletionHandler.bind(null, rowData.id)}
-         />
-      );
+   function deletionButton(data) {
+      return <DeletionButton onClick={deletionHandler.bind(null, data.id)} />;
    }
 
    function deletionHandler(selectedNutrientId) {
@@ -160,10 +141,10 @@ export default function NutrientList(props) {
                onRowEditComplete={rowEditCompletionHandler}
                responsiveLayout='scroll'
             >
-               {numberColumn}
+               <Numbers header='No' body={rowData => rowData.index + 1} />
                {nutrientListColumns}
-               {auth.isAdminLoggedIn && editionColumn}
-               {auth.isAdminLoggedIn && deletionColumn}
+               {auth.isAdminLoggedIn && <Edition header='Edit' rowEditor />}
+               {auth.isAdminLoggedIn && <Deletion header='Delete' body={rowData => deletionButton(rowData)} />}
             </DataTable>
          </div>
          <AddNewNutrient
