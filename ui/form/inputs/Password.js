ea59139150f2390e1a6a33594e256fcd1a23c4af
Erdem Ceylan <erdemxceylan@gmail.com>
2022-07-26 19:51:29 +0300

    password visibility logic is completed

--- ui/form/inputs/Password.js
+++ ui/form/inputs/Password.js
@@ -1,19 +1,26 @@
+import { useState } from 'react';
 import { Field } from 'formik';
 import { InputText } from 'primereact/inputtext';
 import styles from './Password.module.css';
 
+const PASSWORD = 'password';
+const TEXT = 'text';
+
 export default function Password(props) {
-   const { className, name, type, ...rest } = props;
+   const { className, name, ...rest } = props;
+   const [type, setType] = useState(PASSWORD);
 
    return (
       <Field name={name} >
          {({ field }) => (
-            <div className={styles.password}>
-               <InputText className={className} type={type} {...field} {...rest} />
-               <i className='pi pi-eye' />
+            <div className={styles.container}>
+               <InputText className={className} {...field} {...rest} type={type} />
+               <i
+                  className={type === PASSWORD ? 'pi pi-eye' : 'pi pi-eye-slash'}
+                  onClick={() => setType(state => state === PASSWORD ? TEXT : PASSWORD)}
+               />
             </div>
-         )
-         }
+         )}
       </Field>
    );
 }
