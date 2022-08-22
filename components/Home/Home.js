a1e9bebfc9a9c3c9203c4836959d4dde96c905df
Erdem Ceylan <erdemxceylan@gmail.com>
2022-08-22 09:04:28 +0300

    lang is suspended

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,14 +3,12 @@ import { useSelector } from 'react-redux';
 import AddNutrientMenu from './utils/AddNutrientMenu';
 import ConsumedNutrientsTable from './utils/ConsumedNutrientsTable';
 import DataMenu from './utils/DataMenu';
-import Lang from './utils/Lang';
 
 export default function Home(props) {
    const isEmpty = useSelector(state => state.consumedNutrients.isEmpty);
 
    return (
       <Fragment>
-         <Lang />
          <DataMenu dataSettings={props.dataSettings} />
          <AddNutrientMenu nutrients={props.nutrients} />
          {!isEmpty && <ConsumedNutrientsTable dailyTargetValues={props.dailyTargetValues} />}
